008d9366bea315dd64bdad36892af560bf1cec61
JoseCusimayta <josecusimaytar@gmail.com>
2017-08-17 11:48:20 -0500

    Jardin Agregado
    Se ha preparado una especie de boceto para el escenario del jardín

---
+++ Assets/Prefabs/Escenario/DuplicarSprites.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuplicarSprites : MonoBehaviour {
+    public Transform Objeto;
+    public int RepeticionesX;
+    public int RepeticionesY;
+    public float DistanciaX;
+    public float DistanciaY;
+    // Use this for initialization
+    void Start () {
+		for ( int x=0; x < RepeticionesX; x++)
+        {
+            for(int y = 0; y < RepeticionesY; y++)
+            {
+                Transform instancia;
+                instancia = Instantiate(Objeto, new Vector3(x * DistanciaX, y * DistanciaY, 0), Quaternion.identity) as Transform;
+                instancia.parent = this.transform;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
