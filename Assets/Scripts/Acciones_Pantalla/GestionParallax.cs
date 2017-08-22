26701b70216e975b915a260b017d27d358d8389d
Paolo Marquez <j18.14p.91m@gmail.com>
2017-08-22 02:15:20 -0500

    - Efecto Parallax en el background
    En la escena de la calle se agrego un efecto parallax, aunque todavia
    faltan mas ajustes.

---
+++ Assets/Scripts/Acciones_Pantalla/GestionParallax.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestionParallax : MonoBehaviour {
+
+    public FreeParallax parallax;
+    public GameObject cloud;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (cloud != null)
+        {
+            cloud.GetComponent<Rigidbody2D>().velocity = new Vector2(0.1f, 0.0f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (parallax != null)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                parallax.Speed = 15.0f;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                parallax.Speed = -15.0f;
+            }
+            else
+            {
+                parallax.Speed = 0.0f;
+            }
+        }
+    }
+}
