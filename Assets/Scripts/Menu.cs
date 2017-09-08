f0d9fbcaf72bfd92ddc4d840afa8cd17b126aa70
asdad <asdadsad@gmail.com>
2017-09-07 21:07:08 -0500

    LuisVicente
    Sistema de menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class Menu : MonoBehaviour {
+	public string nivel;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void CambioDeEscena(){
+	
+		Application.LoadLevel ("Level1");
+
+	}
+
+	public void Salir(){
+		Application.Quit ();
+	}
+
+}
