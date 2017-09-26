099ad64d1d6ccd3bf46bb98dd8b41f06469f78dc
Paolo Marquez <j18.14p.91m@gmail.com>
2017-09-25 23:59:51 -0500

    Pantalla Inicial
    Ahora la pantalla de inicio, es mas dinamica y se mueve al inicial un
    nivel.

--- Assets/Scripts/Acciones_Pantalla/ScreenInicial.cs
+++ Assets/Scripts/Acciones_Pantalla/ScreenInicial.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ScreenInicial : MonoBehaviour {
 	bool activa;
 	public RectTransform panelInicio;
+    public float velocidadTraslado = 10f;
 	// Use this for initialization
 	void Start () {
 		//ajuste inicial que permite que el panel de pausa solo aparesca cuando se le llame por evento
@@ -14,9 +15,18 @@ public class ScreenInicial : MonoBehaviour {
 	}
 
 	void irSalida(){
-		//ajuste inicial que permite que el panel de pausa solo aparesca cuando se le llame por evento
-		panelInicio.localPosition = Vector3.one * 999;
-		activa=true;
+        //ajuste inicial que permite que el panel de pausa solo aparesca cuando se le llame por evento
+        //for(int i = 1; i <= 100; i++)
+        //{
+        //    panelInicio.transform.Translate(Vector3.right*Time.deltaTime*i);
+        //}
+        if (panelInicio.transform.position.x < 999)
+        {
+            panelInicio.GetComponent<Rigidbody2D>().velocity = Vector3.right * velocidadTraslado;
+        }
+        
+        //panelInicio.localPosition = Vector3.one * 999;
+        activa =true;
 
 
 	}
