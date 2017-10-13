523a33deaafa73f57b7ed3728f1eac66b3dc1657
paolo1891 <j18.14p.91m@gmail.com>
2017-10-12 21:05:59 -0500

    Ejemplo Animacion en escenario

---
+++ Assets/Scripts/Acciones_Pantalla/ReproductorGif.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReproductorGif : MonoBehaviour {
+	int index;
+	public int FramesPorSegundo=10;
+	public Texture2D[] Frames;
+	public Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+		rend.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		index=(int)(Time.time*FramesPorSegundo)%Frames.Length;
+		rend.material.mainTexture = Frames[index];
+	}
+
+}
