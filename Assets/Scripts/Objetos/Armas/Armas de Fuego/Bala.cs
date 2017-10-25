4cf3f3a5c56c758cfcdb57b1203c80bccc4dd6e6
paolo1891 <j18.14p.91m@gmail.com>
2017-10-24 20:54:07 -0500

    Avance de balas luminosas

--- Assets/Scripts/Objetos/Armas/Armas de Fuego/Bala.cs
+++ Assets/Scripts/Objetos/Armas/Armas de Fuego/Bala.cs
@@ -34,6 +34,7 @@ public class Bala : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
+		Debug.Log ("Hice colision");
         if (other.CompareTag(Target_Tag))
         {
             morir();
