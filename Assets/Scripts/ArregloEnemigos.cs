04680c3cb947f6ed4cb4bd475bfe537158e1f6df
Paolo Marquez <j18.14p.91m@gmail.com>
2017-10-27 23:19:35 -0500

    Portal hacia subnivel ajustado
    -AHora sólo matando a todos los enemigos de un nivel se puede acceder al
    siguiente.

--- Assets/Scripts/ArregloEnemigos.cs
+++ Assets/Scripts/ArregloEnemigos.cs
@@ -4,31 +4,45 @@ using UnityEngine;
 
 public class ArregloEnemigos : MonoBehaviour {
 	public GameObject[] enemigos;
-	public GameObject portal;
+	
 	bool desactivarMuros;
 	public GameObject pantallaFinal;
+    private int contadorMuertes;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        contadorMuertes = 0;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        muereEnemigo();
+        desactivar_Muros();
+
+    }
 
 	public void muereEnemigo() {
-		for (int i = 0; i < enemigos.Length; i++) {
-			if (enemigos[i]==null) {
-				desactivarMuros = true;
-				break;
+        contadorMuertes = 0;
+
+        for (int i = 0; i < enemigos.Length; i++) {
+			if (enemigos[i].GetComponentInChildren<Guardia>()==null) {
+                contadorMuertes++;
+               
+                Debug.Log("muertes totales= "+ contadorMuertes);
+				
 			}
 		}
+        if (contadorMuertes== enemigos.Length)
+        {
+            Debug.Log("Se activa portal");
+            desactivarMuros = true;
+        }
 	}
 
 	public void desactivar_Muros() {
 		if (desactivarMuros) {
 			pantallaFinal.SetActive (true);
+
 		}
 	}
 
